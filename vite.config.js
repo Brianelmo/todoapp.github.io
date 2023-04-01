35cc8119ab7a8ae9728a3ff55d6ac4bb367e12c6
Brian Elmo <brianelmo14@gmail.com>
2023-03-31 21:23:55 -0300

    base update2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()], 
-  base : 'https://Brianelmo/todoapp.github.io'  
+  base : 'https://brianelmo.github.io/todoapp.github.io/'  
 })
